d5fbe080c2c99e8092274f2f62169423e7c5b6bb
Jon Gill <jonpgill1@gmail.com>
2020-02-05 20:52:43 +0000

    Render google map in map section

---
+++ assets/js/maps.js
@@ -0,0 +1,10 @@
+function initMap() {
+
+    var map = new google.maps.Map(document.getElementById("map"),{
+         zoom: 3,
+         center: {
+             lat: 53.383331,
+             lng: -1.466667
+         }
+    });
+}
