79684f43f17255f1baa93bcd11bb031c484e75ad
Jon Gill <jonpgill1@gmail.com>
2020-06-22 16:28:18 +0000

    Add function to hide searchbar on scroll

--- assets/js/totop.js
+++ assets/js/totop.js
@@ -11,10 +11,23 @@ function scrollFunction() {
   } else {
     totop.style.display = "none";
   }
-}
+};
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+//Hide search bar
+
+searchBar = document.getElementById("search");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+if (document.body.scrollTop >= 1900|| document.documentElement.scrollTop >= 1900) {
+    searchBar.style.display = "none";
+  } else {
+    searchBar.style.display = "block";
+  }
 }
